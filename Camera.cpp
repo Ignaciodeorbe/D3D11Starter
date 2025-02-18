0dad8b54416eb1db1c1cfe09373ef7e40d232216
Ignaciodeorbe <114196026+Ignaciodeorbe@users.noreply.github.com>
2025-02-18 15:01:43 -0500

    Started populating the camera.cpp file

---
+++ Camera.cpp
@@ -0,0 +1,19 @@
+#include "Camera.h"
+
+using namespace DirectX;
+
+Camera::Camera(XMFLOAT3 position, float movmentSpeed, float mouseSpeed, float fov, float aspectRatio)
+{
+
+}
+
+void Camera::UpdateViewMatrix()
+{
+}
+
+void Camera::UpdateProjectionMatrix(float aspectRatio)
+{
+}
+
+XMFLOAT4X4 Camera::ViewMatrix() { return viewMatrix; }
+XMFLOAT4X4 Camera::ProjectionMatrix() { return projectionMatrix; }
