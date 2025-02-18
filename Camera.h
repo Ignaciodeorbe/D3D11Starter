95c5ca90a25b3b1ea97e1d11ac5dab126ec869bf
Ignaciodeorbe <114196026+Ignaciodeorbe@users.noreply.github.com>
2025-02-18 14:42:26 -0500

    Populated the camera header file

--- Camera.h
+++ Camera.h
@@ -3,3 +3,44 @@
 #include "Input.h";
 #include <DirectXMath.h>
 #include "Transform.h"
+
+class Camera
+{
+private:
+	// Variables
+	Transform transform;
+
+	DirectX::XMFLOAT4X4 viewMatrix;
+	DirectX::XMFLOAT4X4 projectionMatrix;
+
+	float fov;
+	float nearClip;
+	float farClip;
+	float speed;
+	float mouseSpeed;
+
+	// Enum for projection type
+	enum class ProjectionType {
+		Perspective,
+		Orthographic
+	};
+
+	ProjectionType projectionType;
+
+	
+public:
+	Camera(DirectX::XMFLOAT3 position, float movmentSpeed, float mouseSpeed, float fov, float aspectRatio);
+
+
+	//--------
+	// Methods
+	// -------
+	void UpdateViewMatrix();
+	void UpdateProjectionMatrix(float aspectRatio);
+
+	//--------
+	// Getters
+	// -------
+	DirectX::XMFLOAT4X4 ViewMatrix();
+	DirectX::XMFLOAT4X4 ProjectionMatrix();
+};
