4b6a1bad717c21cdb9947164d566c64aa56efab9
Ignaciodeorbe <114196026+Ignaciodeorbe@users.noreply.github.com>
2026-02-21 19:11:58 -0500

    Revert "Added material class and updated it to DX12"
    This reverts commit acf5f51a386f17c2e252286e3a67a5402be4d990.

---
+++ Material.cpp
@@ -0,0 +1,92 @@
+#include "Material.h"
+
+
+Material::Material(DirectX::XMFLOAT4 tint, std::shared_ptr<SimpleVertexShader> vertexShader, 
+	std::shared_ptr<SimplePixelShader> pixelShader, DirectX::XMFLOAT2 scale, 
+	DirectX::XMFLOAT2 offset, float distortionStrength, float time, float roughness)
+	: tint(tint), vertexShader(vertexShader), pixelShader(pixelShader), scale(scale), offset(offset), distortionStrength(distortionStrength), time(time), roughness(roughness)
+{
+}
+
+//--------
+// Getters
+//--------
+DirectX::XMFLOAT4 Material::Tint() { return tint; }
+DirectX::XMFLOAT2 Material::Scale() { return scale; }
+DirectX::XMFLOAT2 Material::Offset() { return offset; }
+float Material::DistortionStrength() { return distortionStrength; }
+float Material::Time() { return time; }
+std::shared_ptr<SimpleVertexShader> Material::VertexShader() { return vertexShader; }
+std::shared_ptr<SimplePixelShader> Material::PixelShader() { return pixelShader; }
+
+Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> Material::GetTextureSRV()
+{
+	// Search for texture
+	auto it = textureSRVs.find(shaderName);
+
+	// Return the found texture
+	if (it != textureSRVs.end())
+	{
+		return it->second;  
+	}
+
+	return nullptr;
+}
+
+std::string Material::ShaderName() { return shaderName; }
+
+
+//--------
+// Setters
+//--------
+void Material::SetTint(DirectX::XMFLOAT4 t) { tint = t; }
+void Material::SetScale(DirectX::XMFLOAT2 s) { scale = s; }
+void Material::SetOffset(DirectX::XMFLOAT2 o) { offset = o; }
+void Material::SetDistortionStrength(float distortion) { distortionStrength = distortion; }
+void Material::SetTime(float t) { time = t; }
+void Material::SetVertexShader(std::shared_ptr<SimpleVertexShader> vs) { vertexShader = vs; }
+void Material::SetPixelShader(std::shared_ptr<SimplePixelShader> ps) { pixelShader = ps; }
+
+void Material::AddTextureSRV(std::string shaderVariableName, Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> srv)
+{
+	textureSRVs.insert({ shaderVariableName, srv });
+	shaderName = shaderVariableName;
+
+}
+
+void Material::AddSampler(std::string shaderVariableName, Microsoft::WRL::ComPtr<ID3D11SamplerState> sampler)
+{
+	samplers.insert({ shaderVariableName, sampler });
+
+}
+
+
+void Material::PrepareMaterial(std::shared_ptr<Camera> camera, std::shared_ptr<Transform> transform, float totalTime)
+{
+
+	vertexShader->SetShader();
+	pixelShader->SetShader();
+
+	vertexShader->SetFloat4("colorTint", tint); // Strings here MUST
+	vertexShader->SetMatrix4x4("world", transform->GetWorldMatrix()); // match variable
+	vertexShader->SetMatrix4x4("view", camera->ViewMatrix()); // names in your
+	vertexShader->SetMatrix4x4("projection", camera->ProjectionMatrix()); // shader’s cbuffer!
+	vertexShader->SetMatrix4x4("worldInvTranspose", transform->GetWorldInverseTranspose());
+
+	pixelShader->SetFloat4("colorTint", tint);
+	pixelShader->SetFloat2("scale", scale);
+	pixelShader->SetFloat2("offset", offset);
+	pixelShader->SetFloat("distortionStrength", distortionStrength);
+	pixelShader->SetFloat("time", totalTime);
+	pixelShader->SetFloat("roughness", roughness);
+	pixelShader->SetFloat3("cameraPosition", camera->GetTransform().GetPosition());
+	pixelShader->CopyAllBufferData();
+	vertexShader->CopyAllBufferData();
+
+	for (auto& t : textureSRVs) { pixelShader->SetShaderResourceView(t.first.c_str(), t.second); }
+	for (auto& s : samplers) { pixelShader->SetSamplerState(s.first.c_str(), s.second); }
+
+	pixelShader->SetFloat4("colorTint", tint);
+
+}
+
---
+++ Material.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <d3d11.h>
+#include <DirectXMath.h>
+#include <memory>
+#include "SimpleShader.h"
+#include <unordered_map>
+#include "Camera.h"
+#include "Transform.h"
+
+class Material
+{
+private:
+	DirectX::XMFLOAT4 tint;
+	DirectX::XMFLOAT2 scale;
+	DirectX::XMFLOAT2 offset;
+	float distortionStrength;
+	float time;
+	float roughness;
+	std::shared_ptr<SimpleVertexShader> vertexShader;
+	std::shared_ptr<SimplePixelShader> pixelShader;
+
+	std::unordered_map<std::string, Microsoft::WRL::ComPtr<ID3D11ShaderResourceView>> textureSRVs;
+	std::unordered_map<std::string, Microsoft::WRL::ComPtr<ID3D11SamplerState>> samplers;
+
+	std::string shaderName;
+
+
+public:
+	Material(DirectX::XMFLOAT4 tint, 
+		std::shared_ptr<SimpleVertexShader> vertexShader, 
+		std::shared_ptr<SimplePixelShader> pixelShader,
+		DirectX::XMFLOAT2 scale,
+		DirectX::XMFLOAT2 offset,
+		float distortionStrength = 1.0f,
+		float time = 0.0f,
+		float roughness = 0.5f);
+
+
+	//--------
+	// Getters
+	// -------
+	DirectX::XMFLOAT4 Tint();
+	DirectX::XMFLOAT2 Scale();
+	DirectX::XMFLOAT2 Offset();
+	float DistortionStrength();
+	float Time();
+	float Roughness();
+	std::shared_ptr<SimpleVertexShader> VertexShader();
+	std::shared_ptr<SimplePixelShader> PixelShader();
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> GetTextureSRV();
+	std::string ShaderName();
+
+
+	//--------
+	// Setters
+	//--------
+	void SetTint(DirectX::XMFLOAT4 tint);
+	void SetScale(DirectX::XMFLOAT2 scale);
+	void SetOffset(DirectX::XMFLOAT2 offset);
+	void SetDistortionStrength(float distortionStrength);
+	void SetTime(float time);
+	void SetRoughness(float roughness);
+	void SetVertexShader(std::shared_ptr<SimpleVertexShader> vertexShader);
+	void SetPixelShader(std::shared_ptr<SimplePixelShader> pixelShader);
+
+	//--------
+	// Methods
+	//--------
+	void AddTextureSRV(std::string shaderVariableName, Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> srv);
+	void AddSampler(std::string shaderVariableName, Microsoft::WRL::ComPtr<ID3D11SamplerState> sampler);
+	void PrepareMaterial(std::shared_ptr<Camera> camera, std::shared_ptr<Transform> transform, float deltaTime);
+
+};
